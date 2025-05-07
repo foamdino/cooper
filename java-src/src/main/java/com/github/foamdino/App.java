4f924a39448f719f725a420821c40ac860f3e895
Kev Jackson <foamdino@gmail.com>
2025-05-07 08:55:50 +0100

    fix App, add mvn wrapper

--- java-src/src/main/java/com/github/foamdino/App.java
+++ java-src/src/main/java/com/github/foamdino/App.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Application {
+public class App {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		ApplicationContext ctx = SpringApplication.run(App.class, args);
 
 		System.out.println("Let's inspect the beans provided by Spring Boot:");
 
