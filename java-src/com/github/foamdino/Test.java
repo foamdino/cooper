ee09e534d5b5d5240d53776b8523291306dc75bc
Kev Jackson <foamdino@gmail.com>
2025-05-07 08:20:47 +0100

    wip: create a better test app

--- java-src/com/github/foamdino/Test.java
+++ java-src/com/github/foamdino/Test.java
@@ -1,6 +1,34 @@
 package com.github.foamdino;
 
-/* Simple test class for connectinog agent to */
+/* Just a test class that we can instantiate to trigger object allocation events */
+class Junk {
+
+    private String name;
+    private Long id;
+
+    public Junk(String name, Long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
+
+/* Simple test class for connecting agent to */
 public class Test {
 
     int c(int foo, String bar) throws Exception {
@@ -12,7 +40,19 @@ public class Test {
     }
 
     private String a() {
-        return "return from private method a()";
+        var message = new String();
+        message += "return ";
+        message += "from ";
+        message += "private ";
+        message += "method ";
+        message += " a()";
+
+        var j = new Junk("Test", new Long(1));
+        var j2 = new Junk("Another test", new Long(2));
+        j2.setId(new Long(10));
+        j2.setName("Changed name");
+
+        return message;
     }
 
     public void foo() throws Exception {
