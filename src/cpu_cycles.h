d80c4d087920775e719ca1d34474dc8581d88dac
Kev Jackson <foamdino@gmail.com>
2025-05-10 01:40:30 +0100

    wip: start on getting cycle count from cpu

---
+++ src/cpu_cycles.h
@@ -0,0 +1,89 @@
+/*
+ * SPDX-FileCopyrightText: (c) 2025 Kev Jackson <foamdino@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CPU_CYCLES_H
+#define CPU_CYCLES_H
+
+#include <stdint.h>
+
+#ifdef __x86_64__
+/**
+ * 
+ */
+static __inline__ uint64_t rdtsc_start(void)
+{
+    /* rdtsc instruction returns a 64bit value split across 2 32bit registers
+    edx for the high bits
+    eax for the low bits
+    */
+    unsigned cycles_high_bits; 
+    unsigned cycles_low_bits;
+    /* Keep the code as is, do not optimise away etc */
+    __asm__ __volatile__ (
+        "CPUID\n\t"  /*Serialize - CPUID prevents the processor from re-ordering the RDTSC instruction */
+        "RDTSC\n\t" /*Read Time Stamp Counter instruction */
+        "mov %%edx, %0\n\t" /* move the content of edx into the first output variable - cycle_high_bits */
+        "mov %%eax, %1\n\t" /* move the content of eax into the second output variable - cycle_low_bits */
+        : "=r" (cycles_hig_bits), "=r" (cycles_low_bits) /* Output operands */
+        : /* no input operands */
+        : "%rax", "%rbx", "%rcx", "%rdx"); /* List of 'clobbered' registers  (CPUID  modifies the 64bit version rax etc)*/
+
+    return ((uint64_t)cycles_high_bits << 32) | cycles_low_bits;
+}
+
+/**
+ * 
+ */
+static __inline__ uint64_t rdtsc_end(void)
+{
+
+}
+#endif
+
+#ifdef __aarch64__
+
+static inline uint64_t read_cntvct(void)
+{
+    uint64_t cnt;
+    asm volatile("mrs %0, cntvct_el0" : "=r"(cnt));
+    return cnt;
+}
+
+static inline uint32_t read_cntfrq(void)
+{
+    uint32_t frq;
+    asm volatile("mrs %0, cntfrq_el0" : "=r(frq)");
+    return frq;
+}
+#endif
+
+/**
+ * Call to get the CPU cycle count from cpu registers
+ */
+static __inline__ uint64_t cycles_start(void)
+{
+#ifdef __x86_64__
+    return rdtsc_start();
+#endif
+#ifdef __aarch64__
+    return read_cntvct();
+#endif
+}
+
+/**
+ * Call to get the CPU cycle count from cpu registers
+ */
+static __inline__ uint64_t cycles_end(void)
+{
+#ifdef __x86_64__
+    return rdtsc_end();
+#endif
+#ifdef __aarch64__
+    return read_cntvct();
+#endif
+}
+
+#endif
